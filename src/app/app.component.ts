9db60c8345315fe0c72af87712422e083e6c3790
luciuses <luciuses@gmail.com>
2017-05-21 21:10:31 +0300

    added login service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {LoginComponent} from './login/login.component';
+import {LoginService} from './login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoginService]
 })
 export class AppComponent {
   title = 'app works!';
+  //login = new LoginData();
+  constructor(private loginService: LoginService) {
+  }
+
+  login() {
+    this.loginService.login();
+  }
 }
---
+++ src/app/login.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LoginService {
+
+  constructor() { }
+  login(){
+    console.log("login exec!");
+	  alert('6uttuihg');
+  }
+}
